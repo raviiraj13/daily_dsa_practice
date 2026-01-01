9b22c69d2f7e315929bee5c007958695296d24b1
Ravi Raj <raviraj2024jm@gmail.com>
2026-01-02 01:32:23 +0530

    Update array_sorted_ornot.cpp

--- Day1/array_sorted_ornot.cpp
+++ Day1/array_sorted_ornot.cpp
@@ -14,3 +14,40 @@ int main(){
     cout<<"nums is sorted : "<<check(nums);
      return 0;
 }
+
+
+//method1
+
+
+//for leetcode
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int size = nums.size();
+//         int durandhar = 0;
+//         for(int i = 0;i < nums.size();i++){
+//             if(nums[i]>nums[(i+1) % size]) durandhar++;
+//         }
+//         if( durandhar > 1) return false;
+//         else return true;
+//     }
+// };
+
+
+//method 2
+
+
+// for leetcode
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int check=0;
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i]>nums[i+1]) check++;
+//         }
+//         int j=nums.size()-1;
+//         if(nums[j]>nums[0]) check++;
+//         if(check >= 2) return false;
+//         else return true;
+//     }
+// };
